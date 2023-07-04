9cc418b8a41c940b29dca1a07cd35dd7560c3522
Mojoj97 <nzsombor97.hu@hmail.com>
2023-07-04 22:40:56 +0200

    Creating RestAPI for Product and Product category

--- src/main/java/com/webshop/ecommerce/SpringBootEcommerceApplication.java
+++ src/main/java/com/webshop/ecommerce/SpringBootEcommerceApplication.java
@@ -1,7 +1,12 @@
 package com.webshop.ecommerce;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+
 
 @SpringBootApplication
 public class SpringBootEcommerceApplication {
